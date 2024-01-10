d7b31b5feeb750619fe81ff1f4f8a911051c16f7
David Gao <david.04.gao@gmail.com>
2024-01-10 15:06:37 -0500

    initial commit pull block

---
+++ Assets/Scripts/PullBlock.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Block))]
+
+public class PullBlock : Block
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+}
